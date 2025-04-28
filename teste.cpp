d44903457e31a94ed8d8448ae6668e5aab9393c2
Mateus <mateus.souza.137@ufrn.edu.br>
2025-04-27 23:45:42 -0300

    primeiro programa depois de meses

---
+++ teste.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int a, b, c;
+int main()  {
+     cout << "Digite o valor de a e de b" << endl;
+     cin >> a >> b;
+     c = a + b;
+     cout << "o valor de a+b é igual a" << c << "\n" << endl;
+return 0;
+
+}
